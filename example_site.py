3b9f714b069f15b4e8b71d9900571f20f2300f50
Bimalkant Lauhny <lauhny.bimalk@gmail.com>
2018-07-31 21:57:00 +0530

    Separate Client js and css files

--- example_site.py
+++ example_site.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template_string
 from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
@@ -13,45 +14,14 @@ def hello():
                 <meta charset="utf-8" />
                 <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <style type="text/css">
-                body {
-                    background-color: #f0f0f2;
-                    margin: 0;
-                    padding: 0;
-                    font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
-
-                }
-                div {
-                    width: 600px;
-                    margin: 5em auto;
-                    padding: 50px;
-                    background-color: #fff;
-                    border-radius: 1em;
-                }
-                a:link, a:visited {
-                    color: #38488f;
-                    text-decoration: none;
-                }
-                @media (max-width: 700px) {
-                    body {
-                        background-color: #fff;
-                    }
-                    div {
-                        width: auto;
-                        margin: 0 auto;
-                        border-radius: 0;
-                        padding: 1em;
-                    }
-                }
-                </style>
+                <link rel="stylesheet" type="text/css" href="static/css/client.css">
             </head>
-
             <body>
                 <div>
                     <h1>Example Site</h1>
                     <p>Every instance of this site will execute some payload and return the results back.</p><br>
                 </div>
-                <script type="text/javascript" src="static/client.js"></script>
+                <script type="text/javascript" src="static/javascript/client.js"></script>
             </body>
             </html>
         """)
